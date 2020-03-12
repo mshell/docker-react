9ad574f51c937c4613f72f06ef20fd36a4d4a67a
Martin Shell <martin@osiopt.com>
2020-03-12 15:16:19 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-          Hi there!
-        </p>
+        More changes
+         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
